62060381c86ce0dc82dd42849c681002c1437080
Eric Budai <eric_budai@hotmail.com>
2023-01-17 21:10:24 -0500

    better error for bad syntax in main parser loop

--- Compiler/Grammar/Errors/UnexpectedSymbolError.cs
+++
@@ -1,8 +0,0 @@
-﻿using Ronin.Compiler;
-
-namespace Ronin.Grammar.Errors;
-
-internal class UnexpectedSymbolError : Error, IParsable
-{
-    public static Syntax Parse(ref Parser context) => Parse<UnexpectedSymbolError>(ref context);
-}
---
+++ Compiler/Grammar/Errors/UnexpectedSyntaxError.cs
@@ -0,0 +1,8 @@
+﻿using Ronin.Compiler;
+
+namespace Ronin.Grammar.Errors;
+
+internal class UnexpectedSyntaxError : Error, IParsable
+{
+    public static Syntax Parse(ref Parser context) => Parse<UnexpectedSyntaxError>(ref context);
+}
