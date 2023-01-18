415d9b386826389dc53b4b44f99eab6f008222eb
Eric Budai <eric_budai@hotmail.com>
2023-01-17 21:10:36 -0500

    added bad function name test

---
+++ Test/Failure/Function.cs
@@ -0,0 +1,27 @@
+﻿using Ronin.Compiler;
+using Ronin.Grammar;
+using Ronin.Grammar.Errors;
+
+namespace Failure;
+
+[Trait("Parser", null)]
+public class Function
+{
+    [Fact(DisplayName = "bad name")]
+    public void BadName()
+    {
+        const string line = "function test) thing(x => number) { }";
+
+        Lexer lexer = new(line);
+        var tokens = lexer.Lex();
+        Parser parser = new(tokens);
+        var statements = parser.Parse();
+
+        Assert.NotEmpty(statements);
+        var statement = statements[0] as Statement;
+        Assert.Null(statement);
+        var error = statements[0] as Error;
+        Assert.NotNull(error);
+        Assert.IsType<UnexpectedSyntaxError>(error);
+    }
+}
