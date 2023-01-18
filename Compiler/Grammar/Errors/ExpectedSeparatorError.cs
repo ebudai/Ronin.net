95d4413459ce5b3eba70c7cca29bc335cd3f1db1
Eric Budai <eric_budai@hotmail.com>
2023-01-17 21:43:08 -0500

    added new error

---
+++ Compiler/Grammar/Errors/ExpectedSeparatorError.cs
@@ -0,0 +1,8 @@
+﻿using Ronin.Compiler;
+
+namespace Ronin.Grammar.Errors;
+
+internal class ExpectedSeparatorError : Error, IParsable
+{
+    public static Syntax Parse(ref Parser context) => Parse<ExpectedSeparatorError>(ref context);
+}
