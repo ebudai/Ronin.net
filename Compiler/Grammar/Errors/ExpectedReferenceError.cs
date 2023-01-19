4907092eb84193693c339d0a570ac591a33b834b
Eric Budai <eric_budai@hotmail.com>
2023-01-19 06:08:45 -0500

    no longer using expected reference error

--- Compiler/Grammar/Errors/ExpectedReferenceError.cs
+++
@@ -1,8 +0,0 @@
-﻿using Ronin.Compiler;
-
-namespace Ronin.Grammar.Errors;
-
-internal class ExpectedReferenceError : Error, IParsable
-{
-    public static Syntax Parse(ref Parser context) => Parse<ExpectedReferenceError>(ref context);
-}
