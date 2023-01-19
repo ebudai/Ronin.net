1ba686065d25f33f9af80089ff6c8129ec6ee717
Eric Budai <eric_budai@hotmail.com>
2023-01-19 05:27:48 -0500

    added comments and copyright

--- Compiler/Grammar/Aggregates/Arguments.cs
+++ Compiler/Grammar/Aggregates/Arguments.cs
@@ -1,8 +1,22 @@
-﻿using Ronin.Lexicon.Symbols;
+﻿// Copyright © 2023 Eric Budai
+
+using Ronin.Lexicon.Symbols;
 
 namespace Ronin.Grammar.Aggregates;
 
+/// <summary>
+///     Comma-separated list of values in parenthesis
+/// </summary>
+/// 
+/// <remarks>
+///     This is used to supply values to <see cref="Parameters"/>
+/// </remarks>
+/// 
+/// <example>
+///     (a, 8.2, "first name")
+/// </example>
+/// 
 internal class Arguments : Aggregate<Arguments, OpenParenthesis, Value, Separator, CloseParenthesis>
 {
-    //public override string ToString() => '(' + string.Join(",", Values) + ')';
+    
 }
