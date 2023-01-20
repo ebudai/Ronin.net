7aaeb8947e34042a6c8780ab72b7222da1a1263f
Eric Budai <eric_budai@hotmail.com>
2023-01-20 06:54:05 -0500

    assignment branch covered 100%

---
+++ Test/Failure/Assignment.cs
@@ -0,0 +1,28 @@
+﻿using Ronin.Compiler;
+using Ronin.Grammar.Errors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Failure;
+
+[Trait("Parser", null)]
+public class Assignment
+{
+    [Fact(DisplayName = "no value")]
+    public void NoValue()
+    {
+        const string declaration = "x =;";
+
+        Lexer lexer = new(declaration);
+        var tokens = lexer.Lex();
+        Parser parser = new(tokens);
+        var statements = parser.Parse();
+
+        Assert.Empty(statements);
+        Assert.NotNull(parser.Errors);
+        Assert.IsType<UnexpectedSyntaxError>(parser.Errors[0]);
+    }
+}
