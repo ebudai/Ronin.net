5cd09379b692c0e5d36bbe887e4e6aa078826b69
Eric Budai <eric_budai@hotmail.com>
2023-02-03 06:52:32 -0500

    lexer removed from ordinal parsing tests

--- Test/Unit/Ordinal.cs
+++ Test/Unit/Ordinal.cs
@@ -1,27 +1,38 @@
 ﻿using Ronin.Compiler;
 using Ronin.Grammar;
+using Ronin.Lexicon;
+using Ronin.Lexicon.Keywords;
+using Ronin.Lexicon.Literals;
+using Ronin.Lexicon.Symbols;
+using Test;
 
 namespace Unit;
 
 [Trait("Parser", null)]
-public class Ordinal
+#pragma warning disable CS8981
+#pragma warning disable IDE1006
+public class ordinal
 {
     [Fact(DisplayName = "basic")]
     public void Basic()
     {
-        const string line = "var test => integer[4];";
+        Tokens tokens = new();
+        tokens.Add<Variable>()
+            .Add<Word>("test")
+            .Add<Returns>()
+            .Add<Word>("number")
+            .Add<OpenSquareBracket>()
+            .Add<Number>("4")
+            .Add<CloseSquareBracket>()
+            .Add<Terminal>();
 
-        Lexer lexer = new(line);
-        var tokens = lexer.Lex();
-        Parser parser = new(tokens);
-        var statements = parser.Parse();
+        Parser parser = new(tokens.ToArray());
+        var datum = Datum.Parse(ref parser);
 
-        Assert.NotEmpty(statements);
-        Ronin.Grammar.Datum datum = statements[0] as Statement;
         Assert.NotNull(datum?.Datatype?.Ordinal);
-        Assert.NotEmpty(datum.Datatype.Ordinal.Values);
-        Ronin.Grammar.Scalar scalar = datum.Datatype.Ordinal.Values[0];
-        Assert.NotEmpty(scalar.Literals);
+        Assert.Single(datum.Datatype.Ordinal.Values);
+        Scalar scalar = datum.Datatype.Ordinal.Values[0];
+        Assert.Single(scalar.Literals);
         Assert.Equal("4", scalar.Literals[0].ToString());
     }
 }
