47debd28b0635b998ae5622c7696f5f9ab226c62
Eric Budai <eric_budai@hotmail.com>
2023-02-03 06:40:18 -0500

    lexer removed from list parsing tests

--- Test/Unit/List.cs
+++ Test/Unit/List.cs
@@ -1,25 +1,33 @@
 ﻿using Ronin.Compiler;
 using Ronin.Grammar;
+using Ronin.Lexicon;
+using Ronin.Lexicon.Keywords;
+using Ronin.Lexicon.Symbols;
+using Test;
 
 namespace Unit;
 
 [Trait("Parser", null)]
-public class List
+#pragma warning disable CS8981
+#pragma warning disable IDE1006
+public class list
 {
     [Fact(DisplayName = "basic")]
     public void Basic()
     {
-        const string declaration = "var x => integer[];";
+        Tokens tokens = new();
+        tokens.Add<Variable>()
+            .Add<Word>("x")
+            .Add<Returns>()
+            .Add<Word>("number")
+            .Add<OpenSquareBracket>()
+            .Add<CloseSquareBracket>()
+            .Add<Terminal>();
 
-        Lexer lexer = new(declaration);
-        var tokens = lexer.Lex();
-        Parser parser = new(tokens);
-        var syntax = parser.Parse();
+        Parser parser = new(tokens.ToArray());
+        var datum = Datum.Parse(ref parser);
 
-        Assert.NotEmpty(syntax);
-        Ronin.Grammar.Datum datum = syntax[0] as Statement;
-        Assert.NotNull(datum);
-        Assert.NotNull(datum.Datatype);
+        Assert.NotNull(datum?.Datatype?.Ordinal);
         Assert.Empty(datum.Datatype.Ordinal.Values);
     }
 }
