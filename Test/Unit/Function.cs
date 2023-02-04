68c25fb9b9e2b8551ac4b6c643e3e26b90306ee0
Eric Budai <eric_budai@hotmail.com>
2023-02-04 08:11:44 -0500

    function unit tests reduced

--- Test/Unit/Function.cs
+++ Test/Unit/Function.cs
@@ -36,44 +36,22 @@ public class function
         Parser parser = new(tokens.ToArray());
         var function = Function.Parse(ref parser);
         
-        Assert.NotNull(function);
+        Name name = function?.Identifier?.Components?[0];
+        Assert.Equal("test", name?.Words?[0]);
 
-        Assert.NotNull(function.Identifier);
-        Assert.Equal(2, function.Identifier.Components.Count);
+        Parameters parameters = function?.Identifier?.Components?[1];
+        var parameter = parameters?.Values?[0];
+        Assert.Equal("x", parameter?.Name?.Words?[0]);
 
-        Name name = function.Identifier.Components[0];
-        Assert.NotNull(name);
-        Assert.Single(name.Words);
-        Assert.Equal("test", name.Words[0]);
+        Name datatype = parameter?.Datatype?.Components?[0];
+        Assert.Equal("number", datatype?.Words?[0]);
 
-        Parameters parameters = function.Identifier.Components[1];
-        Assert.NotNull(parameters);
-        Assert.Single(parameters.Values);
-        var parameter = parameters.Values[0];
-        Assert.Single(parameter.Name.Words);
-        Assert.Equal("x", parameter.Name.Words[0]);
+        Reference line = function?.Body?.Values?[0];
+        Name @return = line?.Components?[0];
+        Assert.Equal("return", @return?.Words?[0]);
 
-        Assert.Null(function.Modifiers);
-
-        Assert.Single(parameter.Datatype.Components);
-        Name datatype = parameter.Datatype.Components[0];
-        Assert.Single(datatype.Words);
-        Assert.Equal("number", datatype.Words[0]);
-
-        Assert.Single(function.Body.Values);
-        Reference line = function.Body.Values[0];
-        Assert.NotNull(line);
-        Assert.Equal(2, line.Components.Count);
-
-        Name @return = line.Components[0];
-        Assert.NotNull(@return);
-        Assert.Single(@return.Words);
-        Assert.Equal("return", @return.Words[0]);
-
-        Scalar scalar = line.Components[1];
-        Assert.NotNull(scalar);
-        Assert.Single(scalar.Literals);
-        Assert.Equal("7", scalar.Literals[0].ToString());        
+        Scalar scalar = line?.Components?[1];
+        Assert.Equal("7", scalar?.Literals?[0].ToString());        
     }
 
     [Fact(DisplayName = "specifies return datatype")]
@@ -104,45 +82,26 @@ public class function
         Assert.NotNull(function);
         Assert.Equal(2, function.Identifier.Components.Count);
 
-        Name name = function.Identifier.Components[0];
-        Assert.NotNull(name);
-        Assert.Single(name.Words);
-        Assert.Equal("test", name.Words[0]);
+        Name name = function?.Identifier?.Components?[0];
+        Assert.Equal("test", name?.Words?[0]);
 
-        Parameters parameters = function.Identifier.Components[1];
-        Assert.NotNull(parameters);
-        Assert.Single(parameters.Values);
-        var parameter = parameters.Values[0];
-        Assert.Single(parameter.Name.Words);
-        Assert.Equal("x", parameter.Name.Words[0]);
+        Parameters parameters = function?.Identifier?.Components?[1];
+        var parameter = parameters?.Values?[0];
+        Assert.Equal("x", parameter?.Name?.Words?[0]);
 
-        Assert.Single(parameter.Datatype.Components);
-        Name datatype = parameter.Datatype.Components[0];
-        Assert.Single(datatype.Words);
-        Assert.Equal("text", datatype.Words[0]);
+        Name datatype = parameter?.Datatype?.Components?[0];
+        Assert.Equal("text", datatype?.Words?[0]);
 
-        Assert.True(function.Modifiers.Optional);
-        Assert.False(function.Modifiers.Persistent);
-        Assert.False(function.Modifiers.Shared);
-        Assert.False(function.Modifiers.Compiled);
+        Assert.True(function?.Modifiers?.Optional);
+        Assert.False(function?.Modifiers?.Persistent);
+        Assert.False(function?.Modifiers?.Shared);
+        Assert.False(function?.Modifiers?.Compiled);
 
-        Assert.NotNull(function.Returns);
-        Assert.Single(function.Returns.Components);
-        name = function.Returns.Components[0];
-        Assert.Single(name.Words);        
-        Assert.Equal("number", name.Words[0]);
+        Name returns = function?.Returns?.Components?[0];
+        Assert.Equal("number", returns?.Words?[0]);
 
-        Assert.Single(function.Body.Values);
-        Reference line = function.Body.Values[0];
-        Assert.NotNull(line);
-        Assert.Single(line.Components);
-
-        Name @return = line.Components[0];
-        Assert.NotNull(@return);
-        Assert.Equal(4, @return.Words.Count);
-        Assert.Equal("return", @return.Words[0]);
-        Assert.Equal("x", @return.Words[1]);
-        Assert.Equal("as", @return.Words[2]);
-        Assert.Equal("number", @return.Words[3]);
+        Reference line = function?.Body?.Values?[0];
+        Name @return = line?.Components?[0];
+        Assert.Equal("return x as number", string.Join(" ", @return?.Words ?? new List<string>()));
     }
 }
