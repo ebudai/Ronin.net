8424f7cfac7f0edf767bc1d96916359da8e0d1c8
Eric Budai <eric_budai@hotmail.com>
2023-02-14 06:00:21 -0500

    fleshed out Language,Datatype

--- Compiler/Language/Datatype.cs
+++ Compiler/Language/Datatype.cs
@@ -1,6 +1,36 @@
-﻿namespace Ronin.Language;
+﻿using Ronin.Compiler;
+using Ronin.Grammar;
+
+namespace Ronin.Language;
 
 internal class Datatype
 {
+    public Identifier Identifier { get; init; }
+    
+    public List<Datatype> InnerDatatypes { get; } = new();
+    public List<Datum> Data { get; } = new();
+    public List<Function> Operations { get; } = new();
 
+    public List<Datatype> Parents { get; } = new();
+    public List<Datatype> Unions { get; } = new();
+
+    public static Datatype Analyze(ref SemanticAnalyzer analyzer)
+    {
+        if (analyzer.CurrentSyntax is not Grammar.Datatype type) return null;
+
+        UnresolvedDatatype datatype = new() { Identifier = type.Identifier, Algebra = type.Algebra };
+
+        foreach (var statement in type.Body.Values)
+        {
+            
+        }        
+
+        return datatype;
+    }
+}
+
+internal class UnresolvedDatatype : Datatype
+{
+    public Reference Reference { get; init; }
+    public Reference Algebra { get; init; }
 }
--- Compiler/SemanticAnalyzer.cs
+++ Compiler/SemanticAnalyzer.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Ronin.Grammar;
 
-namespace Ronin
+namespace Ronin.Compiler;
+
+internal class SemanticAnalyzer
 {
-    internal class SemanticAnalyzer
-    {
-    }
+    public Syntax CurrentSyntax => null;
 }
