b3908999e6764d6b7df2fff498cb14af0b7817c1
Eric Budai <eric_budai@hotmail.com>
2023-06-07 07:28:36 -0400

    renamed SemanticAnalyzer to Analyzer

---
+++ Compiler/Analyzer.cs
@@ -0,0 +1,11 @@
+﻿using Ronin.Grammar.Compound;
+using Ronin.Language;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Ronin;
+
+[ExcludeFromCodeCoverage]
+internal class Analyzer
+{
+    public static Context Define(Definition scope) => new(scope, Context.Global, true);
+}
