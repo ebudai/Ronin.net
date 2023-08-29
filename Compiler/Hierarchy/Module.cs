0d018c42ec73aee59128e0a0168e662144ab0141
Eric Budai <eric_budai@hotmail.com>
2023-08-29 06:12:30 -0400

    initial hierarchy stuff (for reference resolution)

---
+++ Compiler/Hierarchy/Module.cs
@@ -0,0 +1,31 @@
+﻿using Ronin.Grammar;
+
+namespace Ronin.Hierarchy;
+
+internal class Module : Context
+{
+    public Module() : base() { Parent = Global.Scope; }
+
+    public List<Context> Contexts { get; init; } = new();
+
+    public override Identifier Existing(Identifier identifier)
+    {
+        foreach (var context in Contexts)
+        {
+            if (context.Existing(identifier) is Identifier found) return found;
+        }
+        return null;
+    }
+
+    public override List<Resolution> Resolve(Reference reference)
+    {
+        List<Resolution> resolutions = new();
+
+        foreach (var context in Contexts)
+        {
+            resolutions.AddRange(context.Resolve(reference));
+        }
+
+        return resolutions;
+    }
+}
---
+++ Compiler/Hierarchy/Resolution.cs
@@ -0,0 +1,19 @@
+﻿using Ronin.Grammar;
+
+namespace Ronin.Hierarchy;
+
+internal class Resolution
+{
+    public Context.Member Member { get; set; }
+    public List<Resolution> Resolutions { get; } = new();
+
+    public int Size
+    {
+        get
+        {
+            int count = 1;
+            foreach (var resolution in Resolutions) count += resolution.Size;
+            return count;
+        }
+    }
+}
